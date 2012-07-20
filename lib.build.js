4bc9aa674faefa4ce0edfbb0a1b5a1443f9668fa
paravena <paravena74@gmail.com>
2012-07-20 15:34:40 -0700

    creating an optimizer build file

--- lib.build.js
+++ lib.build.js
@@ -1,13 +1,7 @@
 ({
-    appDir : '.',
     baseUrl : 'scripts',
-    dir : 'build',
     stubModules: ['cs'],
-    mainConfigFile : 'scripts/main.js',
-    modules : [
-        {
-            name : 'main',
-            exclude: ['jquery', 'jquerypp.custom', 'cs']
-        }
-    ]
+    name : 'main',
+    out : 'scripts/main-build.js',
+    exclude: ['cs', 'jquery', 'jquerypp.custom']
 })
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,5 @@
-require(['jquery', 'cs!myui/TextField', 'cs!myui/Autocompleter', 'cs!myui/ComboBox'], function($, TextField, Autocompleter, ComboBox) {
+require(['cs!myui/ComboBox'], function(ComboBox) {
     $(document).ready(function(){
-        /*
-        new TextField({input: '#name',
-            initialText: 'Enter name',
-            required: true,
-            validate : function(value, errors) {
-                var result = true;
-                if (value != 'peter') {
-                    errors.push('bad name');
-                    result = false;
-                }
-                return result;
-            }
-        });
-        */
         window.comboBox = new ComboBox({
             input : '#name',
             url : 'get_manufacturers_list.php',
