f86a3a66f7dfff100f2b3948c3b8085e2fdd2cec
Pablo Aravena <paravena74@gmail.com>
2015-01-02 09:50:46 -0800

    samples sever will allows easier testing

---
+++ samples/samples_server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(req, res) {
+    var filePath = req.url;
+    if (filePath === '/favicon.ico') {
+        return res.end();
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    filePath = filePath.replace('/', path.sep);
+    setTimeout(function() {
+        var pathToFile = __dirname + filePath;
+        console.log('reading file ' + pathToFile);
+        fs.readFile(pathToFile, {
+           encoding: 'utf8'
+        },
+        function(error, content) {
+            if (error) {
+                console.error(error);
+                res.end();
+            } else {
+                res.end(content);
+            }
+        });
+    }, 5000);
+}).listen(3000);
